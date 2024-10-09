d4589643afdcd0dc6224e187772884e335c77b19
crdx <pub@crdx.org>
2024-10-09 22:38:17 +0100

    Use numbered parameters

--- lib/soml/util.rb
+++ lib/soml/util.rb
@@ -1,14 +1,14 @@
 module SOML
     class Util
         def self.dedent(str)
-            indentation_level = str.lines.map { |line| line[/^\s*/].length }.min
+            indentation_level = str.lines.map { _1[/^\s*/].length }.min
             return str if indentation_level == 0
 
-            return str.lines.map { |line| line[indentation_level..] }.join
+            return str.lines.map { _1[indentation_level..] }.join
         end
 
         def self.indent(str, level: 4)
-            str.lines.map { |line| ' ' * level + line }.join
+            str.lines.map { ' ' * level + _1 }.join
         end
     end
 end
